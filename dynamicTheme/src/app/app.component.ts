f73d1464d029cd438ff1fe18cc93c7980101810d
Salini Vijayan <amsalinivijayan@gmail.com>
2021-11-25 10:03:34 +0530

    added 2 more colors

--- dynamicTheme/src/app/app.component.ts
+++ dynamicTheme/src/app/app.component.ts
@@ -6,18 +6,19 @@ import { AppState } from './app.global';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+
   public appPages = [
     { title: 'Default Red', theme: 'theme-red', color:'assets/imgs/FF0000.png' },
     { title: 'Noir', theme: 'theme-noir', color:'assets/imgs/333333.png' },
-    // { title: 'Clover', theme: 'theme-clover', color:'assets/imgs/388E3C.png' },
-    // { title: 'Blueberry', theme: 'theme-blueberry', color:'assets/imgs/1e88e5.png' }
+    { title: 'Clover', theme: 'theme-clover', color:'assets/imgs/388E3C.png' },
+    { title: 'Blueberry', theme: 'theme-blueberry', color:'assets/imgs/1e88e5.png' }
   ];
-  // public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
-  constructor( public global:AppState) {}
 
+  constructor( public global:AppState ) {}
 
   changeTheme(theme:any){
-    console.log("Now Changing theme to "+ theme);
+    // console.log("Now Changing theme to "+ theme);
     this.global.set('theme', theme);
   }
+
 }
